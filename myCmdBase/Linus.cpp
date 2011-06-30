e81f72c4e9b381c95be87e5553d7ba27479ac61a
JeongKeun Park <pjk41018@gmail.com>
2011-07-01 03:02:30 +0900

    Linus commint

---
+++ myCmdBase/Linus.cpp
@@ -0,0 +1,8 @@
+#include "Linus.h"
+
+int Linus(int argc, char (*argv)[ARG_SIZE]){
+	cout<<"What should I Do?"<<endl;
+	cout<<"Please tell me"<<endl;
+	cout<<"How to uses argc, argv :-X"<<endl;
+	return 0;
+}
